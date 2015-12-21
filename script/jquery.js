087fd961f8f84086a36e78cc66a57a3ca1960a85
Liam Chapman <Liam@Liams-MBP.home>
2015-12-21 01:37:43 +0000

    With random chords

--- script/jquery.js
+++ script/jquery.js
@@ -5,3 +5,12 @@ $(function(){
                     trigger: "click"
                     });
 });
+
+
+var intervalID = setInterval(function(){
+                             var chords = ['A','C','D','E'];
+                             var randIndex = 0 + Math.floor(Math.random() * chords.length)
+                             $("#frontCard").attr("src","./img/chord" + chords[randIndex] + "Text.svg");
+                             $("#backCard").attr("src","./img/chord" + chords[randIndex] + "Shape.svg");
+                             console.log("Chord: " + chords[randIndex]);
+}, 10000 + Math.floor(Math.random() * 25000));
